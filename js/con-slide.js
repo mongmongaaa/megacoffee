b84d140f14e6eb2bc0e7d83fe2df3774b3f6dd4b
yong-hoon <133857448+yonghoon1013@users.noreply.github.com>
2023-07-24 18:06:22 +0900

    tt
    t

--- js/con-slide.js
+++ js/con-slide.js
@@ -16,8 +16,7 @@ var swiper = new Swiper(".mySwiper", {
 
 
 var swiper = new Swiper(".menu_ul", {
-    slidesPerView: 2,
-    spaceBetween: 15,
+    spaceBetween: 10,
     // loop: true,
     pagination: {
         el: ".swiper-pagination",
@@ -28,5 +27,28 @@ var swiper = new Swiper(".menu_ul", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+    breakpoints: {
+       
+       
+          364: { //브라우저가 364보다 클 떄
+            slidesPerView: 4,
+            slidesPerGroup: 2,
+          },
+          760: { //브라우저가 760보다 클 떄
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+          }
+      },
+    //   breakpoints: {
+        
+    //     768: {
+    //       slidesPerView: 2,  //브라우저가 768보다 클 때
+    //       spaceBetween: 40,
+    //     },
+    //     1024: {
+    //       slidesPerView: 3,  //브라우저가 1024보다 클 때
+    //       spaceBetween: 50,
+    //     },
+      
 
 });
